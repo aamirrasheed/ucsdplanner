96c33b12ca09d6419ce431e4bfb91fa7a2ade655
Suhas Arehalli <sgarehal@ucsd.edu>
2016-05-13 14:13:00 -0700

    rewrote capescrape.py to do RMP and CAPE for our new professor matching system

--- scrape/capescrape.py
+++ scrape/capescrape.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup 
 import bs4
 import urllib2
-import mechanize
 import cookielib
 import json
 import requests
@@ -9,69 +8,34 @@ import re
 
 BASE_URL="https://cape.ucsd.edu/responses/Results.aspx?Name=&CourseNumber="
 DEPARTMENT_LIST_URL="https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudent.htm"
-
+GET_DEPT_URL = "https://act.ucsd.edu/scheduleOfClasses/department-list.json"
+RATINGS_URL = 'http://www.ratemyprofessors.com/ShowRatings.jsp?tid='
 DB_URL = "https://ucsdplanner-api.azure-mobile.net/api/"
-
-# ========================================
-# Purpose: Starts virtual browser
-# ======================================== 
-def get_virtual_browser():
-
-	# Browser
-	br = mechanize.Browser()
-
-	# Cookie Jar
-	cj = cookielib.LWPCookieJar()
-	br.set_cookiejar(cj)
-
-	# Browser options
-	br.set_handle_equiv(True)
-	br.set_handle_gzip(True)
-	br.set_handle_redirect(True)
-	br.set_handle_referer(True)
-	br.set_handle_robots(False)
-
-	# Follows refresh 0 but not hangs on refresh > 0
-	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-
-	# Want debugging messages?
-	#br.set_debug_http(True)
-	#br.set_debug_redirects(True)
-	#br.set_debug_responses(True)
-
-	# User-Agent (this is cheating, ok?)
-	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-	return br
+RMP_SEARCH = "http://search.mtvnservices.com/typeahead/suggest/?solrformat=true&rows=10&q={fname}+{lname}&defType=edismax&qf=teacherfullname_t%5E1000+autosuggest&bf=pow(total_number_of_ratings_i%2C1.7)&sort=score+desc&siteName=rmp&group=on&group.field=content_type_s&group.limit=20&schoolid_s=1079"
 
 # ========================================
 # Purpose: Gets soup for a website
 # ======================================== 
-def get_soup(url, br):
-	r = br.open(url)
-	html = r.read()
+def get_soup(url):
+        r = requests.get(url, headers={"User-agent":'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'})
+	html = r.text
 	soup = BeautifulSoup(html, "html.parser");
 	return soup
 
 # ========================================
 # Purpose: Gets list of UCSD departments
 # ======================================== 
-def get_departments(br):
-	soup1 = BeautifulSoup('<select id="selectedSubjects" name="selectedSubjects" multiple="multiple" size="19"><option value="AIP ">AIP  - Academic Internship Program   </option><option value="ANBI">ANBI - Anthro/Biological Anthropology</option><option value="ANAR">ANAR - Anthropological Archeology    </option><option value="ANTH">ANTH - Anthropology                  </option><option value="ANSC">ANSC - Anthropology/Sociocultural    </option><option value="AESE">AESE - ArchitectureBsdEntrpSystmsEngr</option><option value="BENG">BENG - Bioengineering                </option><option value="BNFO">BNFO - Bioinformatics                </option><option value="BIEB">BIEB - Biol/Ecology, Behavior, &amp; Evol</option><option value="BICD">BICD - Biol/Genetics,Cellular&amp;Develop</option><option value="BIPN">BIPN - Biology/Animal Physiol&amp;Neurosc</option><option value="BIBC">BIBC - Biology/Biochemistry          </option><option value="BGGN">BGGN - Biology/Grad/General          </option><option value="BGSE">BGSE - Biology/Grad/Seminar          </option><option value="BILD">BILD - Biology/Lower Division        </option><option value="BIMM">BIMM - Biology/Molec Biol, Microbiol </option><option value="BISP">BISP - Biology/Special Studies       </option><option value="BIOM">BIOM - Biomedical Sciences           </option><option value="CENG">CENG - Chemical Engineering          </option><option value="CHEM">CHEM - Chemistry and Biochemistry    </option><option value="CHIN">CHIN - Chinese Studies               </option><option value="CLAS">CLAS - Classical Studies             </option><option value="CLIN">CLIN - Clinical Psychology           </option><option value="COGS">COGS - Cognitive Science             </option><option value="COMM">COMM - Communication                 </option><option value="COGR">COGR - Communication/Graduate        </option><option value="CSE ">CSE  - Computer Science &amp; Engineering</option><option value="ICAM">ICAM - Computing and the Arts        </option><option value="CONT">CONT - Contemporary Issues           </option><option value="CGS ">CGS  - Critical Gender Studies       </option><option value="CAT ">CAT  - Culture, Art, &amp; Technology    </option><option value="TDCH">TDCH - Dance/Choreography            </option><option value="TDHD">TDHD - Dance/History                 </option><option value="TDMV">TDMV - Dance/Movement                </option><option value="TDTR">TDTR - Dance/Theory                  </option><option value="DOC ">DOC  - Dimensions of Culture         </option><option value="ECON">ECON - Economics                     </option><option value="EAP ">EAP  - Education Abroad Program      </option><option value="EDS ">EDS  - Education Studies             </option><option value="ERC ">ERC  - Eleanor Roosevelt College     </option><option value="ECE ">ECE  - Electrical &amp; Computer Engineer</option><option value="ENG ">ENG  - Engineering                   </option><option value="ENVR">ENVR - Environmental Studies         </option><option value="ESYS">ESYS - Environmental Systems         </option><option value="ETHN">ETHN - Ethnic Studies                </option><option value="EXPR">EXPR - Exchange Programs             </option><option value="FPMU">FPMU - Family&amp;Prev Med Undergraduate </option><option value="FILM">FILM - Film Studies                  </option><option value="HITO">HITO - History Topics                </option><option value="HIAF">HIAF - History of Africa             </option><option value="HIEA">HIEA - History of East Asia          </option><option value="HIEU">HIEU - History of Europe             </option><option value="HILA">HILA - History of Latin America      </option><option value="HISC">HISC - History of Science            </option><option value="HINE">HINE - History of the Near East      </option><option value="HIUS">HIUS - History of the United States  </option><option value="HIGR">HIGR - History, Graduate             </option><option value="HILD">HILD - History, Lower Division       </option><option value="HDP ">HDP  - Human Development Program     </option><option value="HUM ">HUM  - Humanities                    </option><option value="INTL">INTL - International Studies         </option><option value="IRCO">IRCO - Intrnat Relat/Pac Study-Core  </option><option value="IRGN">IRGN - Intrnat Relat/Pac Study-Generl</option><option value="IRLA">IRLA - Intrnat Relat/Pac Study-Lang  </option><option value="JAPN">JAPN - Japanese Studies              </option><option value="JUDA">JUDA - Judaic Studies                </option><option value="LATI">LATI - Latin American Studies        </option><option value="LAWS">LAWS - Law and Society               </option><option value="LISL">LISL - Linguistics/Amer Sign Language</option><option value="LIAB">LIAB - Linguistics/Arabic            </option><option value="LIFR">LIFR - Linguistics/French            </option><option value="LIGN">LIGN - Linguistics/General           </option><option value="LIGM">LIGM - Linguistics/German            </option><option value="LIHL">LIHL - Linguistics/Heritage Languages</option><option value="LIIT">LIIT - Linguistics/Italian           </option><option value="LIPO">LIPO - Linguistics/Portuguese        </option><option value="LISP">LISP - Linguistics/Spanish           </option><option value="LTCH">LTCH - Literature/Chinese            </option><option value="LTCO">LTCO - Literature/Comparative        </option><option value="LTCS">LTCS - Literature/Cultural Studies   </option><option value="LTEU">LTEU - Literature/European &amp; Eurasian</option><option value="LTFR">LTFR - Literature/French             </option><option value="LTGM">LTGM - Literature/German             </option><option value="LTGK">LTGK - Literature/Greek              </option><option value="LTIT">LTIT - Literature/Italian            </option><option value="LTKO">LTKO - Literature/Korean             </option><option value="LTLA">LTLA - Literature/Latin              </option><option value="LTRU">LTRU - Literature/Russian            </option><option value="LTSP">LTSP - Literature/Spanish            </option><option value="LTTH">LTTH - Literature/Theory             </option><option value="LTWR">LTWR - Literature/Writing            </option><option value="LTEN">LTEN - Literatures in English        </option><option value="LTWL">LTWL - Literatures of the World      </option><option value="LTEA">LTEA - Literatures/East Asian        </option><option value="MMW ">MMW  - Making of the Modern World    </option><option value="MBC ">MBC  - MarineBiodiversity&amp;Conservatn </option><option value="MATS">MATS - Materials Sci &amp; Engineering   </option><option value="MATH">MATH - Mathematics                   </option><option value="MSED">MSED - Mathematics &amp; Science Educ    </option><option value="MAE ">MAE  - Mechanical &amp; Aerospace Engin  </option><option value="MUIR">MUIR - Muir College                  </option><option value="MCWP">MCWP - Muir College Writing Program  </option><option value="MUS ">MUS  - Music                         </option><option value="NANO">NANO - NanoEngineering               </option><option value="PHAR">PHAR - Pharmacology                  </option><option value="SPPS">SPPS - Pharmacy                      </option><option value="PHIL">PHIL - Philosophy                    </option><option value="PHYS">PHYS - Physics                       </option><option value="POLI">POLI - Political Science             </option><option value="PSYC">PSYC - Psychology                    </option><option value="MGT ">MGT  - Rady School of Management     </option><option value="RELI">RELI - Religion, Study of            </option><option value="REV ">REV  - Revelle College               </option><option value="SDCC">SDCC - San Diego Community College   </option><option value="SIOC">SIOC - Scripps Inst of Oceanogr/COAP </option><option value="SIOG">SIOG - Scripps Inst of Oceanogr/GEO  </option><option value="SIOB">SIOB - Scripps Inst of Oceanogr/OBP  </option><option value="SIO ">SIO  - Scripps Inst of Oceanography  </option><option value="SXTH">SXTH - Sixth College                 </option><option value="SOCG">SOCG - Soc/Graduate                  </option><option value="SOCE">SOCE - Soc/Ind Research &amp; Honors Prog</option><option value="SOCI">SOCI - Sociology                     </option><option value="SE  ">SE   - Structural Engineering        </option><option value="TDAC">TDAC - Theatre / Acting              </option><option value="TDDE">TDDE - Theatre / Design              </option><option value="TDDR">TDDR - Theatre / Directing&amp;Stage Mgmt</option><option value="TDGE">TDGE - Theatre / General             </option><option value="TDGR">TDGR - Theatre / Graduate            </option><option value="TDHT">TDHT - Theatre / History &amp; Theory    </option><option value="TDPW">TDPW - Theatre / Playwriting         </option><option value="TDPR">TDPR - Theatre Dance/Practicum       </option><option value="TWS ">TWS  - Third World Studies           </option><option value="TMC ">TMC  - Thurgood Marshall College     </option><option value="USP ">USP  - Urban Studies &amp; Planning      </option><option value="VIS ">VIS  - Visual Arts                   </option><option value="WARR">WARR - Warren College                </option><option value="WCWP">WCWP - Warren College Writing Program</option></select>', "html.parser")
-	
-	dept_soup = get_soup(DEPARTMENT_LIST_URL, br)
-	working = dept_soup.find(id="selectedSubjects")
-	print working
-	
-	dept_list = [ str(dept['value']) for dept in soup1.find_all('option')]
-	return dept_list
+def get_departments():
+        return ['AIP', 'ANBI', 'ANAR', 'ANTH', 'ANSC', 'AESE', 'BENG', 'BNFO', 'BIEB', 'BICD', 'BIPN', 'BIBC', 'BGGN', 'BGSE', 'BILD', 'BIMM', 'BISP', 'BIOM', 'CENG', 'CHEM', 'CHIN', 'CLAS', 'CLIN', 'COGS', 'COMM', 'COGR', 'CSE', 'ICAM', 'CONT', 'CGS', 'CAT', 'TDCH', 'TDHD', 'TDMV', 'TDTR', 'DOC', 'ECON', 'EAP', 'EDS', 'ERC', 'ECE', 'ENG', 'ENVR', 'ESYS', 'ETHN', 'EXPR', 'FPMU', 'FILM', 'HITO', 'HIAF', 'HIEA', 'HIEU', 'HILA', 'HISC', 'HINE', 'HIUS', 'HIGR', 'HILD', 'HDP', 'HUM', 'INTL', 'IRCO', 'IRGN', 'IRLA', 'JAPN', 'JUDA', 'LATI', 'LAWS', 'LISL', 'LIAB', 'LIFR', 'LIGN', 'LIGM', 'LIHL', 'LIIT', 'LIPO', 'LISP', 'LTCH', 'LTCO', 'LTCS', 'LTEU', 'LTFR', 'LTGM', 'LTGK', 'LTIT', 'LTKO', 'LTLA', 'LTRU', 'LTSP', 'LTTH', 'LTWR', 'LTEN', 'LTWL', 'LTEA', 'MMW', 'MBC', 'MATS', 'MATH', 'MSED', 'MAE', 'MUIR', 'MCWP', 'MUS', 'NANO', 'PHAR', 'SPPS', 'PHIL', 'PHYS', 'POLI', 'PSYC', 'MGT', 'RELI', 'REV', 'SDCC', 'SIOC', 'SIOG', 'SIOB', 'SIO', 'SXTH', 'SOCG', 'SOCE', 'SOCI', 'SE', 'TDAC', 'TDDE', 'TDDR', 'TDGE', 'TDGR', 'TDHT', 'TDPW', 'TDPR', 'TWS', 'TMC', 'USP', 'VIS', 'WARR', 'WCWP']
+
 
 # ========================================
 # Purpose: Gets CAPE Data for a
 # 		   specified department
 # ======================================== 
-def get_cape_data_for_dept(dept, br):
+def get_cape_data_for_dept(dept):
 	# gets soup
-	soup = get_soup(BASE_URL+dept, br)
+	soup = get_soup(BASE_URL+dept)
 	row = None
 
 	# gets container with row
@@ -149,31 +113,41 @@ def get_cape_data_for_dept(dept, br):
 		graderec = working_col.span.contents[0]
 
                 
-                # Get ids for tables before posting
-                ln, fn = instructor.split(", ")
+                # reformat instructor names
+                names = instructor.split(", ")
+                ln = names[0]
+                fn = " ".join(names[1:])
                 instructor = fn.strip() + " " + ln.strip()
+                lname = ln
+                fname = names[1].split(" ")[0]
                 
+                prof_id = get_RMP_prof(fname, lname) 
+                
+                # TODO: Update to match new term-binding when possible 
+                # Get or make term 
                 r = requests.get(DB_URL + "term/" + term)
                 terms = json.loads(r.text)
-                
                 if terms == []:
                     pass #r = requests.post(DB_URL, parseTerm(term))
-                # Ensure that terms are unique
-                assert len(terms) <= 1
+                if len(terms) > 1:
+                    print "ERROR: Duplicate Terms: " + str(terms) 
                 #term_id = terms[0]["id"] 
 
                 r = requests.get(DB_URL + "catalog/" + course)
                 courses = json.loads(r.text)
+
                 if len(courses) == 0:
+                    print"WARNING: Could not find course: " + course
                     row = row.next_sibling
                     continue
 
-                assert len(courses) == 1
+                if len(courses) > 1:
+                    print "ERROR: Duplicate Courses: " + str(courses) 
                 catalog_id = courses[0]["id"]
 
 		# enter data into dictionary
 		entry = {
-			'instructor': instructor,
+			'professor_id': prof_id,
 			'catalog_id': catalog_id,
                         #'term_id': term_id,
                         'term': term,
@@ -186,13 +160,11 @@ def get_cape_data_for_dept(dept, br):
 			'cape_prof_gpa': graderec
 		}
 		
+                # TODO: uncomment posting to DB
+                #r = requests.post(DB_URL + "/cape", entry)
 		# add entry to list
 		cape_data.append(entry)
-                print(entry)
 		row = row.next_sibling
-		# Debug
-		# row_num = row_num + 1;
-		# print row_num
 	return cape_data
 
 def parseTerm(term):
@@ -210,13 +182,57 @@ def parseTerm(term):
     out["year"] = "20" + split.group(2)
     return out
 
+def get_RMP_prof(fname, lname):
+    r = requests.get(RMP_SEARCH.format(fname = fname, lname = lname))
+
+    profs = json.loads(r.text)["grouped"]["content_type_s"]["groups"]
+    if len(profs) == 0:
+        return 0
+    profs = profs[0]["doclist"]["docs"]
+
+    profs = list(filter(lambda x: x["schoolname_s"] == "University of California San Diego" , profs))
+
+    if len(profs) == 0:
+        return 0
+    else:
+        tid = profs[0]["pk_id"]
+        r = requests.get(RATINGS_URL + tid)
+
+        soup = BeautifulSoup(req.text, "html.parser")
+        header = soup.find("div", "left-breakdown")
+
+        professor = {}
+        # if header is None, then there are no ratings for prof
+        if header == None:
+            professor['name'] = prof['name']
+            professor['rmp_overall'] = None
+            professor['rmp_helpful'] = None
+            professor['rmp_clarity'] = None
+            professor['rmp_easiness'] = None
+            professor['rmp_hot'] = None
+
+        else:
+            overall_rating = soup.find("div", "grade").string
+            chili_figure = [grade.figure for grade in soup.findAll("div", "grade")]
+            chili_figure = chili_figure[2].img.get('src')
+
+            ratings = soup.findAll("div", "rating")
+            helpfulness = ratings[0].string
+            clarity = ratings[1].string
+            easiness = ratings[2].string
+
+            # dictionary of prof ratings to be put in data
+            professor['name'] = prof['name']
+            professor['rmp_overall'] = overall_rating
+            professor['rmp_helpful'] = helpfulness
+            professor['rmp_clarity'] = clarity
+            professor['rmp_easiness'] = easiness
+            professor['rmp_hot'] = 1 if chili_figure != COLD_CHILI_PIC else 0
+
+        r.post(DB_URL + "professor", professor)
+        return json.loads(r.text)["id"]
 
 if __name__ == "__main__":
-    # initialize virtual browser
-    br = get_virtual_browser()
-
-    print 'started browser'
-
     # Debug Only
     # cape_data = get_cape_data_for_dept("CSE", br)
     # print cape_data
@@ -251,17 +267,18 @@ if __name__ == "__main__":
     # }
     #
     # ===========================================
+
     # dict to hold ALL the cape data
     capes_by_departments = {}
 
     # get list of departments
-    departments = get_departments(br)
+    departments = get_departments()
 
     # get capes for each department
     for dept in departments:
-            cape_data = get_cape_data_for_dept(dept, br)
+            cape_data = get_cape_data_for_dept(dept)
             capes_by_departments[dept] = cape_data
-
+            print "{} of {}".format(gotten, attempts)
     # output to fat ass json file
     with open('cape_data.json', 'w') as fp:
         json.dump(capes_by_departments, fp)
--- scrape/csvTodb.py
+++ scrape/csvTodb.py
@@ -1,16 +1,23 @@
 import csv
 import requests
 import json
+import re
+
+def split_course_name(name):
+    match = re.match("([A-Z]*)(.*)", name)
+    return " ".join(match.groups())
+
 
 # TERM stuff
-TERM_API = "https://ucsdplanner-api.azure-mobile.net/api/term/" 
+DB_URL = "https://ucsdplanner-api.azure-mobile.net/api" 
+
 term_data = {
     "term_name":"FA16",
     "quarter":"Fall",
     "year":"2016"
 }
 
-r = requests.get(TERM_API + term_data["term_name"])
+r = requests.get(DB_URL + "/term", term_data["term_name"])
 
 if r.text == "[]":
     r = requests.post(TERM_API, term_data)
@@ -20,11 +27,35 @@ with open("courses.csv", "r") as f_courses, \
      open("sections.csv", "r") as f_sections, \
      open("exams.csv") as f_exams:
 
-    courses = csv.DictReader(f_courses)
-    sections = csv.DictReader(f_sections)
-    exams = csv.DictReader(f_examss)
+    courses = list(csv.DictReader(f_courses))
+    sections = list(csv.DictReader(f_sections))
+    exams = list(csv.DictReader(f_examss))
+
+    sections_dict = {key:[x for x in sections if x["Id"] == key] for key in range(len(courses))}
+    exams_dict = {key:[x for x in exams if x["Id"] == key] for key in range(len(courses))}
 
     for course in courses:
-        `
+
+        course_dict = {}
+
+        course_name = split_course_name(course["Code"])
+        r = requests.get(DB_URL + "/catalog/" + course_name)
+        
+        catalog_course_id = json.loads(r.text)["catalog_id"]
+
+        r = requests,get(DB_URL "/courses/" + catalog_course_id + "/" + term_id)
+
+        course_id = None
+        if r.text != "[]":
+            course_id = json.loads(r.text)["course_id"]
+        else:
+            course_dict["term_id"] = term_id
+            course_dict["course_name"] = course_name
+            course_dict["catalog_course_id"] = catalog_course_id
+            r = requests.post(DB_URL + "/courses", course_dict)
+            course_id = json.loads(r.text)["course_id"]
+
+        
+        
 
     
