59fa70625d4abb945ea7a817d5d557638609b74b
SahilBansal235 <sahilbansal1796@gmail.com>
2016-05-17 22:26:49 -0700

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+var express = require('express');
+var config = require('./config/config');
+var static = require('serve-static')
+var join = require('path').join
 
+var app = express();
+app.use(static(join(__dirname,'public/js')));
+app.use(static(join(__dirname,'public/css')));
+app.use(static(join(__dirname,'app/views')));
+app.use(static(join(__dirname,'config')));
+app.listen(config.port, function () {
+  console.log('Express server listening on port ' + config.port);
+});
 
+/*
 var express = require('express'),
   config = require('./config/config');
 
@@ -10,4 +23,4 @@ require('./config/express')(app, config);
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
-
+*/
